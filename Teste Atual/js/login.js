aafd4fe6e644d80a0815e7c8371af65e6332b797
Luiz Felipe <sayychr@gmail.com>
2025-06-23 09:37:18 -0400

    Add files via upload

---
+++ Teste Atual/js/login.js
@@ -0,0 +1,43 @@
+let button = document.querySelector("#btnEnviar");
+
+button.addEventListener("click", async function(){
+    let usuario = {
+        nome: document.querySelector("#usuario").value,
+        senha: document.querySelector("#senha").value
+    }
+
+    let loginInfo = await AuxiFunc.validateUser(usuario.nome, usuario.senha);
+
+    if(loginInfo.getNivel != 0){
+        if(loginInfo.getReset){
+            const boxReset = document.querySelector(".reset-box");
+            boxReset.style.display = "flex";
+
+            document.querySelector(".reset-login").addEventListener("click", async function(){
+                let senhaInfo = {
+                novSenha: document.querySelector(".rest_senha").value,
+                confSenha: document.querySelector(".reset_conf").value
+                }
+                if(senhaInfo.novSenha === senhaInfo.confSenha){
+                    if(await AuxiFunc.changePass(loginInfo.getId, senhaInfo.novSenha)){
+                        alert("Senha alterada");
+                        boxReset.style.display = "none";
+                        window.location.href = "pagina_inicial.html";
+                    }else{
+                        alert("Deu tudo errado nessa porra");
+                    }
+
+                }else{
+                    alert("As senhas não conferem");
+                }
+            })
+            
+        }else{
+            window.location.href = "pagina_inicial.html";
+        }
+        sessionStorage.setItem("nivel", loginInfo.getNivel.toString());
+        
+    }else{
+        alert("Usuário ou senha incorretos");
+    }
+})
---
+++ Teste Atual/js/perfil_menu.js
@@ -0,0 +1,21 @@
+
+function toggleMenu() {
+    const menu = document.getElementById('menu-perfil');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+}
+let link = document.querySelector(".menu-registro");
+link.addEventListener("click", function(){
+    if(Number(sessionStorage.getItem("nivel")) < 2){
+        alert("Você não tem permissão para isso")
+    }else{
+        window.location.href = "cadastrar_ususario.html";
+    }
+})
+// Fecha o menu ao clicar fora
+document.addEventListener('click', function(event) {
+    const menu = document.getElementById('menu-perfil');
+    const perfil = document.querySelector('.foto-perfil');
+    if (menu && !perfil.contains(event.target)) {
+        menu.style.display = 'none';
+    }
+});
