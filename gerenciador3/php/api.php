5d5446ade68eff2bb3626885a216cc6717be3930
Luiz Felipe <sayychr@gmail.com>
2025-07-17 14:06:00 -0400

    (Nome não Definido)
    snmpwalk
    
    
    -da funcionalidade pros cards das impressoras
    - ajeita o sistema de login
    - mudar o designer de alteração de senha
    - da funcionalidade a colocar foto de perfil no site

--- gerenciador3/php/api.php
+++ gerenciador3/php/api.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=UTF-8");
 
 $method = $_SERVER['REQUEST_METHOD'];
-$conn = new mysqli("127.0.0.1", "root", "", "gerenciamento");
+$conn = new mysqli("127.0.0.1", "root", "", "testelogin");
 
 if ($conn->connect_error) {
     http_response_code(500);
